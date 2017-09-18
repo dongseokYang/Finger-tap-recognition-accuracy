2eca94a84febc7017c2ed3baa6cd67bad5c4434b
DESKTOP-8I5797C\mldon <mldongseok@gmail.com>
2017-09-18 23:40:05 +0900

    no message

---
+++ TypingPractice/Form1.Designer.cs
@@ -0,0 +1,340 @@
+﻿namespace TypingPractice
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label_current = new System.Windows.Forms.Label();
+            this.label_forward = new System.Windows.Forms.Label();
+            this.label_backward = new System.Windows.Forms.Label();
+            this.label_true = new System.Windows.Forms.Label();
+            this.label_false = new System.Windows.Forms.Label();
+            this.label_tpm = new System.Windows.Forms.Label();
+            this.pic_tf = new System.Windows.Forms.PictureBox();
+            this.pic_hand = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pic_finger0 = new System.Windows.Forms.PictureBox();
+            this.pic_finger1 = new System.Windows.Forms.PictureBox();
+            this.pic_finger2 = new System.Windows.Forms.PictureBox();
+            this.pic_finger3 = new System.Windows.Forms.PictureBox();
+            this.pic_finger4 = new System.Windows.Forms.PictureBox();
+            this.pic_finger5 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pic_tf)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pic_hand)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pic_finger0)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pic_finger1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pic_finger2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pic_finger3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pic_finger4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pic_finger5)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 25;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label_current
+            // 
+            this.label_current.AutoSize = true;
+            this.label_current.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label_current.Font = new System.Drawing.Font("굴림", 30F, System.Drawing.FontStyle.Bold);
+            this.label_current.ForeColor = System.Drawing.Color.Black;
+            this.label_current.Location = new System.Drawing.Point(510, 79);
+            this.label_current.MinimumSize = new System.Drawing.Size(260, 60);
+            this.label_current.Name = "label_current";
+            this.label_current.Size = new System.Drawing.Size(260, 60);
+            this.label_current.TabIndex = 0;
+            this.label_current.Text = "backspace";
+            this.label_current.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label_forward
+            // 
+            this.label_forward.AutoSize = true;
+            this.label_forward.Font = new System.Drawing.Font("굴림", 28F);
+            this.label_forward.ForeColor = System.Drawing.Color.Black;
+            this.label_forward.Location = new System.Drawing.Point(795, 89);
+            this.label_forward.MaximumSize = new System.Drawing.Size(450, 40);
+            this.label_forward.MinimumSize = new System.Drawing.Size(450, 40);
+            this.label_forward.Name = "label_forward";
+            this.label_forward.Size = new System.Drawing.Size(450, 40);
+            this.label_forward.TabIndex = 1;
+            this.label_forward.Text = "▶  A  ▶  Backspace";
+            this.label_forward.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // label_backward
+            // 
+            this.label_backward.AutoEllipsis = true;
+            this.label_backward.AutoSize = true;
+            this.label_backward.Font = new System.Drawing.Font("굴림", 22F);
+            this.label_backward.ForeColor = System.Drawing.Color.DarkGray;
+            this.label_backward.Location = new System.Drawing.Point(49, 89);
+            this.label_backward.MaximumSize = new System.Drawing.Size(450, 40);
+            this.label_backward.MinimumSize = new System.Drawing.Size(450, 40);
+            this.label_backward.Name = "label_backward";
+            this.label_backward.Size = new System.Drawing.Size(450, 40);
+            this.label_backward.TabIndex = 2;
+            this.label_backward.Text = "A  ▶  Backspace  ▶";
+            this.label_backward.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // label_true
+            // 
+            this.label_true.AutoSize = true;
+            this.label_true.Font = new System.Drawing.Font("굴림", 20F);
+            this.label_true.ForeColor = System.Drawing.Color.Black;
+            this.label_true.Location = new System.Drawing.Point(924, 592);
+            this.label_true.MinimumSize = new System.Drawing.Size(50, 40);
+            this.label_true.Name = "label_true";
+            this.label_true.Size = new System.Drawing.Size(100, 40);
+            this.label_true.TabIndex = 5;
+            this.label_true.Text = "정답: 0";
+            this.label_true.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // label_false
+            // 
+            this.label_false.AutoSize = true;
+            this.label_false.Font = new System.Drawing.Font("굴림", 20F);
+            this.label_false.ForeColor = System.Drawing.Color.Black;
+            this.label_false.Location = new System.Drawing.Point(1079, 592);
+            this.label_false.MinimumSize = new System.Drawing.Size(50, 40);
+            this.label_false.Name = "label_false";
+            this.label_false.Size = new System.Drawing.Size(100, 40);
+            this.label_false.TabIndex = 7;
+            this.label_false.Text = "오답: 0";
+            this.label_false.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // label_tpm
+            // 
+            this.label_tpm.AutoSize = true;
+            this.label_tpm.Font = new System.Drawing.Font("굴림", 20F);
+            this.label_tpm.ForeColor = System.Drawing.Color.Black;
+            this.label_tpm.Location = new System.Drawing.Point(240, 28);
+            this.label_tpm.MinimumSize = new System.Drawing.Size(800, 40);
+            this.label_tpm.Name = "label_tpm";
+            this.label_tpm.Size = new System.Drawing.Size(800, 40);
+            this.label_tpm.TabIndex = 8;
+            this.label_tpm.Text = "정답: 0";
+            this.label_tpm.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // pic_tf
+            // 
+            this.pic_tf.Image = global::TypingPractice.Properties.Resources.circle_disable;
+            this.pic_tf.Location = new System.Drawing.Point(929, 339);
+            this.pic_tf.Name = "pic_tf";
+            this.pic_tf.Size = new System.Drawing.Size(250, 250);
+            this.pic_tf.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pic_tf.TabIndex = 6;
+            this.pic_tf.TabStop = false;
+            // 
+            // pic_hand
+            // 
+            this.pic_hand.Image = global::TypingPractice.Properties.Resources.handR_t;
+            this.pic_hand.Location = new System.Drawing.Point(0, 0);
+            this.pic_hand.Name = "pic_hand";
+            this.pic_hand.Size = new System.Drawing.Size(460, 540);
+            this.pic_hand.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pic_hand.TabIndex = 4;
+            this.pic_hand.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(615, 138);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(50, 20);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pic_finger0
+            // 
+            this.pic_finger0.BackColor = System.Drawing.Color.Transparent;
+            this.pic_finger0.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pic_finger0.BackgroundImage")));
+            this.pic_finger0.ErrorImage = null;
+            this.pic_finger0.Image = global::TypingPractice.Properties.Resources.circle_disable;
+            this.pic_finger0.InitialImage = null;
+            this.pic_finger0.Location = new System.Drawing.Point(12, 299);
+            this.pic_finger0.Name = "pic_finger0";
+            this.pic_finger0.Size = new System.Drawing.Size(50, 50);
+            this.pic_finger0.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pic_finger0.TabIndex = 9;
+            this.pic_finger0.TabStop = false;
+            // 
+            // pic_finger1
+            // 
+            this.pic_finger1.BackColor = System.Drawing.Color.Transparent;
+            this.pic_finger1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pic_finger1.BackgroundImage")));
+            this.pic_finger1.ErrorImage = null;
+            this.pic_finger1.Image = global::TypingPractice.Properties.Resources.circle_disable;
+            this.pic_finger1.InitialImage = null;
+            this.pic_finger1.Location = new System.Drawing.Point(60, 243);
+            this.pic_finger1.Name = "pic_finger1";
+            this.pic_finger1.Size = new System.Drawing.Size(50, 50);
+            this.pic_finger1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pic_finger1.TabIndex = 10;
+            this.pic_finger1.TabStop = false;
+            // 
+            // pic_finger2
+            // 
+            this.pic_finger2.BackColor = System.Drawing.Color.Transparent;
+            this.pic_finger2.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pic_finger2.BackgroundImage")));
+            this.pic_finger2.ErrorImage = null;
+            this.pic_finger2.Image = global::TypingPractice.Properties.Resources.circle_disable;
+            this.pic_finger2.InitialImage = null;
+            this.pic_finger2.Location = new System.Drawing.Point(142, 62);
+            this.pic_finger2.Name = "pic_finger2";
+            this.pic_finger2.Size = new System.Drawing.Size(50, 50);
+            this.pic_finger2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pic_finger2.TabIndex = 11;
+            this.pic_finger2.TabStop = false;
+            // 
+            // pic_finger3
+            // 
+            this.pic_finger3.BackColor = System.Drawing.Color.Transparent;
+            this.pic_finger3.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pic_finger3.BackgroundImage")));
+            this.pic_finger3.ErrorImage = null;
+            this.pic_finger3.Image = global::TypingPractice.Properties.Resources.circle_disable;
+            this.pic_finger3.InitialImage = null;
+            this.pic_finger3.Location = new System.Drawing.Point(240, 21);
+            this.pic_finger3.Name = "pic_finger3";
+            this.pic_finger3.Size = new System.Drawing.Size(50, 50);
+            this.pic_finger3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pic_finger3.TabIndex = 12;
+            this.pic_finger3.TabStop = false;
+            // 
+            // pic_finger4
+            // 
+            this.pic_finger4.BackColor = System.Drawing.Color.Transparent;
+            this.pic_finger4.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pic_finger4.BackgroundImage")));
+            this.pic_finger4.ErrorImage = null;
+            this.pic_finger4.Image = global::TypingPractice.Properties.Resources.circle_disable;
+            this.pic_finger4.InitialImage = null;
+            this.pic_finger4.Location = new System.Drawing.Point(317, 49);
+            this.pic_finger4.Name = "pic_finger4";
+            this.pic_finger4.Size = new System.Drawing.Size(50, 50);
+            this.pic_finger4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pic_finger4.TabIndex = 13;
+            this.pic_finger4.TabStop = false;
+            // 
+            // pic_finger5
+            // 
+            this.pic_finger5.BackColor = System.Drawing.Color.Transparent;
+            this.pic_finger5.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pic_finger5.BackgroundImage")));
+            this.pic_finger5.ErrorImage = null;
+            this.pic_finger5.Image = global::TypingPractice.Properties.Resources.circle_disable;
+            this.pic_finger5.InitialImage = null;
+            this.pic_finger5.Location = new System.Drawing.Point(405, 133);
+            this.pic_finger5.Name = "pic_finger5";
+            this.pic_finger5.Size = new System.Drawing.Size(50, 50);
+            this.pic_finger5.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pic_finger5.TabIndex = 14;
+            this.pic_finger5.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label1.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.label1.Font = new System.Drawing.Font("굴림", 25F, System.Drawing.FontStyle.Bold);
+            this.label1.Location = new System.Drawing.Point(26, 603);
+            this.label1.MinimumSize = new System.Drawing.Size(216, 95);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(216, 95);
+            this.label1.TabIndex = 16;
+            this.label1.Text = "다시 시작";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.label1.Click += new System.EventHandler(this.btn_reset_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(1280, 720);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pic_finger5);
+            this.Controls.Add(this.pic_finger4);
+            this.Controls.Add(this.pic_finger3);
+            this.Controls.Add(this.pic_finger2);
+            this.Controls.Add(this.pic_finger1);
+            this.Controls.Add(this.pic_finger0);
+            this.Controls.Add(this.label_tpm);
+            this.Controls.Add(this.label_false);
+            this.Controls.Add(this.pic_tf);
+            this.Controls.Add(this.label_true);
+            this.Controls.Add(this.pic_hand);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label_backward);
+            this.Controls.Add(this.label_forward);
+            this.Controls.Add(this.label_current);
+            this.Name = "Form1";
+            this.Text = "Typing Practice";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyUp);
+            ((System.ComponentModel.ISupportInitialize)(this.pic_tf)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pic_hand)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pic_finger0)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pic_finger1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pic_finger2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pic_finger3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pic_finger4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pic_finger5)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label_current;
+        private System.Windows.Forms.Label label_forward;
+        private System.Windows.Forms.Label label_backward;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pic_hand;
+        private System.Windows.Forms.Label label_true;
+        private System.Windows.Forms.PictureBox pic_tf;
+        private System.Windows.Forms.Label label_false;
+        private System.Windows.Forms.Label label_tpm;
+        private System.Windows.Forms.PictureBox pic_finger0;
+        private System.Windows.Forms.PictureBox pic_finger1;
+        private System.Windows.Forms.PictureBox pic_finger2;
+        private System.Windows.Forms.PictureBox pic_finger3;
+        private System.Windows.Forms.PictureBox pic_finger4;
+        private System.Windows.Forms.PictureBox pic_finger5;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ TypingPractice/Form1.cs
@@ -0,0 +1,214 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TypingPractice
+{
+    public partial class Form1 : Form
+    {
+
+        public Form1() { InitializeComponent(); }
+        private const int count = 10000;
+        private int trueCount = 0;
+        private DateTime timer;
+        private List<Keys> KeyList;
+        private List<Keys> forward;
+        private List<Keys> backward;
+
+        private Keys? pressedKey;
+        private PictureBox[] pic_fingers;
+
+        // Initialize
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            KeyList = new List<Keys>();
+            for (int i = 'A'; i <= 'Z'; i++)
+            {
+                KeyList.Add((Keys)i);
+            }
+            KeyList.Add(Keys.Back);
+            KeyList.Add(Keys.Return);
+            KeyList.Add(Keys.Capital);
+            KeyList.Add(Keys.Space);
+            timer = DateTime.MaxValue;
+
+            forward = new List<Keys>();
+            backward = new List<Keys>();
+
+            pic_fingers = new PictureBox[] { pic_finger0, pic_finger1, pic_finger2, pic_finger3, pic_finger4, pic_finger5 };
+            pic_finger0.Parent = pic_hand;
+            pic_finger1.Parent = pic_hand;
+            pic_finger2.Parent = pic_hand;
+            pic_finger3.Parent = pic_hand;
+            pic_finger4.Parent = pic_hand;
+            pic_finger5.Parent = pic_hand;
+            pic_hand.Location = new Point(390, 180);
+
+            Console.WriteLine("All keys: " + KeyList.Count);
+            Random r = new Random();
+            for (int i = 0; i < count; i++)
+            {
+                Keys key = KeyList[r.Next(0, KeyList.Count)];
+                if(i != 0)
+                {
+                    while(key == forward[i - 1])
+                        key = KeyList[r.Next(0, KeyList.Count)];
+                }
+                forward.Add(key);
+            }
+            Console.WriteLine("Keys generated: " + count);
+            UpdateUI();
+        }
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            Keys key = e.KeyCode;
+            if (key == Keys.Escape) btn_reset_Click(sender, e);
+            if (!KeyList.Contains(key)) return;
+            if (pressedKey != null) return;
+            pressedKey = key;
+
+            if (forward.Count == count) timer = DateTime.Now;
+
+            if (key == forward[0])
+            {
+                trueCount++;
+                pic_tf.Image = Properties.Resources.circle_true;
+            }
+            else
+            {
+                pic_tf.Image = Properties.Resources.circle_false;
+            }
+            backward.Add(forward[0]);
+            forward.RemoveAt(0);
+
+            UpdateUI();
+        }
+        private void Form1_KeyUp(object sender, KeyEventArgs e) { if (pressedKey == e.KeyCode) pressedKey = null; }
+        private void btn_reset_Click(object sender, EventArgs e)
+        {
+            trueCount = 0;
+            timer = DateTime.MaxValue;
+            forward = new List<Keys>();
+            backward = new List<Keys>();
+            Random r = new Random();
+            for (int i = 0; i < count; i++)
+            {
+                Keys key = KeyList[r.Next(0, KeyList.Count)];
+                forward.Add(key);
+            }
+            UpdateUI();
+        }
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            // Update type/min
+            double timeElapsed = ((double)((int)((DateTime.Now - timer).TotalSeconds * 10))) / 10;
+            if (timeElapsed == 0) timeElapsed = 0.000000001;
+            int typePerMin = (int)(trueCount / timeElapsed * 60);
+
+            string s = "타속: " + typePerMin +
+            " | 맞은 개수: " + trueCount +
+            " | 틀린 개수: " + (backward.Count - trueCount);
+            label_tpm.Text = s;
+        }
+        private void UpdateUI()
+        {
+            // Current item text
+            string s = GetName(forward[0]);
+            label_current.Text = s;
+
+            // Forward items text
+            s = "";
+            for (int i = 1; i < forward.Count; i++)
+            {
+                if (i == 4) break;
+                s += "▶  " + GetName(forward[i]) + "  ";
+            }
+            label_forward.Text = s;
+
+            // Backward items text
+            s = "";
+            for (int i = backward.Count - 3; i < backward.Count; i++)
+            {
+                if (i < 0) continue;
+
+                s += "  " + GetName(backward[i]) + "  ▶";
+            }
+            label_backward.Text = s;
+
+            // True count text
+            s = "정답: " + trueCount;
+            label_true.Text = s;
+
+            // False count text
+            s = "오답: " + (backward.Count - trueCount);
+            label_false.Text = s;
+
+            // Key image
+            int[] layer = keyMap[forward[0]];
+            for (int i = 0; i < layer.Length; i++)
+            {
+                if (layer[i] == 0)
+                {
+                    pic_fingers[i].Image = Properties.Resources.circle_disable;
+                }
+                else if (layer[i] == 1)
+                {
+                    pic_fingers[i].Image = Properties.Resources.circle_enable;
+                }
+            }
+        }
+
+
+        private string GetName(Keys key)
+        {
+            if (key == Keys.Return)
+                return "Enter";
+            if (key == Keys.Capital)
+                return "CapsLock";
+            if (key == Keys.Back)
+                return "Backspace";
+
+            return Enum.GetName(typeof(Keys), key);
+        }
+
+        private readonly Dictionary<Keys, int[]> keyMap = new Dictionary<Keys, int[]>()
+        {
+            { Keys.A, new int[]{ 1, 0, 1, 0, 0, 0 } },
+            { Keys.B, new int[]{ 1, 0, 0, 1, 0, 0 } },
+            { Keys.C, new int[]{ 1, 0, 0, 0, 1, 0 } },
+            { Keys.D, new int[]{ 1, 0, 0, 0, 0, 1 } },
+            { Keys.E, new int[]{ 0, 1, 1, 0, 0, 0 } },
+            { Keys.F, new int[]{ 0, 1, 0, 1, 0, 0 } },
+            { Keys.G, new int[]{ 0, 1, 0, 0, 1, 0 } },
+            { Keys.H, new int[]{ 0, 1, 0, 0, 0, 1 } },
+            { Keys.I, new int[]{ 1, 0, 1, 1, 0, 0 } },
+            { Keys.J, new int[]{ 1, 0, 0, 1, 1, 0 } },
+            { Keys.K, new int[]{ 1, 0, 0, 0, 1, 1 } },
+            { Keys.L, new int[]{ 0, 1, 1, 1, 0, 0 } },
+            { Keys.M, new int[]{ 0, 1, 0, 1, 1, 0 } },
+            { Keys.N, new int[]{ 0, 1, 0, 0, 1, 1 } },
+            { Keys.O, new int[]{ 1, 0, 0, 0, 0, 0 } },
+            { Keys.P, new int[]{ 0, 1, 0, 0, 0, 0 } },
+            { Keys.Q, new int[]{ 0, 0, 1, 0, 0, 0 } },
+            { Keys.R, new int[]{ 0, 0, 0, 1, 0, 0 } },
+            { Keys.S, new int[]{ 0, 0, 0, 0, 1, 0 } },
+            { Keys.T, new int[]{ 0, 0, 0, 0, 0, 1 } },
+            { Keys.U, new int[]{ 0, 0, 1, 1, 0, 0 } },
+            { Keys.V, new int[]{ 0, 0, 1, 0, 1, 0 } },
+            { Keys.W, new int[]{ 0, 0, 1, 0, 0, 1 } },
+            { Keys.X, new int[]{ 0, 0, 0, 1, 1, 0 } },
+            { Keys.Y, new int[]{ 0, 0, 0, 1, 0, 1 } },
+            { Keys.Z, new int[]{ 0, 0, 0, 0, 1, 1 } },
+            { Keys.Back, new int[] { 0, 0, 1, 1, 1, 0 } },
+            { Keys.Return, new int[] { 0, 0, 0, 1, 1, 1 } },
+            { Keys.Capital, new int[] { 1, 0, 1, 1, 1, 0 } },
+            { Keys.Space, new int[] { 0, 0, 1, 1, 1, 1 } },
+        };
+    }
+}
